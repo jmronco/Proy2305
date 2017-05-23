10c52ff7923f167b3a7117cc71a3b2e8d15156db
jmronco <>
2017-05-23 11:29:12 -0400

    YEAP_v11

--- lib/Logtransacciones.php
+++ lib/Logtransacciones.php
@@ -1,8 +1,15 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class Logtransacciones{
+    var $idacceso;
+    var $nomtabla;
+    var $accion;
+    var $descripcion;
+    
+    public function __construct1($idacceso="", $nomtabla="", $acccion="", $descripcion) {
+        $this->idacceso = $idacceso;
+        $this->nomtabla = $nomtabla;
+        $this->accion = $acccion;
+        $this->descripcion = $descripcion;
+    }
+}
