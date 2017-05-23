aa30fbac7613ea1aacf27cbe4f19d86d6752f1f9
jmronco <>
2017-05-23 11:15:57 -0400

    YEAP_v8

--- Formulario.php
+++ Formulario.php
@@ -1,10 +1,10 @@
 <html>
     <body>
         <form method="post" action="guardar.php">
-            <div>Nombre: <input type="text" name="nombre"></div>
-            <div>Código: <input type="text" name="código"></div>
-            <div>Precio: <input type="text" name="precio"></div>
-            <input type="submit" value="guardar">
+            <div>Nombre: <input type="text" name="Nombre"></div>
+            <div>Código: <input type="text" name="Código"></div>
+            <div>Precio: <input type="text" name="Precio"></div>
+            <input type="submit" value="Guardar">
                 
         </form>
     </body>
