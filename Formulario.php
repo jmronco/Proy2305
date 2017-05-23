ef57c2a20375c34abc59315a9ceed041b0cdc8b2
jmronco <>
2017-05-23 11:05:55 -0400

    YEAP_v4

---
+++ Formulario.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form method="post" action="guardar.php">
+            <div>nombre: <input type="text" name="nombre"></div>
+            <div>apellido: <input type="text" name="precio"></div>
+            <div>nacimiento: <input type="text" name="código"></div>
+            <input type="submit" value="enviar">
+                
+        </form>
+    </body>
+</html>
+
