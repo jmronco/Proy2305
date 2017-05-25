7382ca04fbd02380abbe486fb6e67209ec593e1f
jmronco <>
2017-05-25 13:36:02 -0400

    Bootstrap v6

--- index.php
+++ index.php
@@ -1,34 +1,8 @@
-<?php
-session_start();
-?>
-
-
-<!DOCTYPE html>
-
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Formulario Usuario</title>
-    </head>
-    <body>
-        <div><?php if(isset($_SESSION['USR'])) { ?>
-            <a href="cerrar.php">Cerrar Sesión</a>
-            <?php } ?>
-        </div>
-        <a href="Formulario.php">Formulario Producto</a>
-        <?php if (!isset($_SESSION['USR'])){?>
-        <form>
-            <div><label>Usuario<input type="text" name="nombre"></div>
-            <div><label>Clave<input type="password" name="clave"></div>
-            <input type="submit" value="Acceder">
-        </form>
-        <?php }
-        ?>
-    </body>
-</html>
-
+</head>
+      <body>
+          <a href="formularios/formcambiarclave.php">Cambiar clave de usuario</a>
+ -        <a href="formularios/formagregarproducto.php"> Agregar Producto</a>
+ +        <a href="formularios/formagregarproducto.php"> Agregar Productos</a>
+          <?php
+          echo PATH;
+          ?>
---
+++ lib/Libreria.php
@@ -0,0 +1,4 @@
+ <?php
+  include (PATH."/dato/Producto.php");
+  include (PATH."/dato/LogTransaccion.php");
+
