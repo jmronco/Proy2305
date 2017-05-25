c4f483bc1c063de1bfbecda84aa988a89e135434
jmronco <>
2017-05-25 12:24:04 -0400

    Bootstrap v4

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ session_start();
 ?>
 
 
-
 <!DOCTYPE html>
+
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -13,17 +13,16 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Formulario Usuario</title>
     </head>
     <body>
         <div><?php if(isset($_SESSION['USR'])) { ?>
             <a href="cerrar.php">Cerrar Sesión</a>
             <?php } ?>
         </div>
-        <a href="revision.php">Inicio de sesi&oacute;n</a>
         <a href="Formulario.php">Formulario Producto</a>
         <?php if (!isset($_SESSION['USR'])){?>
-        <form action="revision.php" method="post">
+        <form>
             <div><label>Usuario<input type="text" name="nombre"></div>
             <div><label>Clave<input type="password" name="clave"></div>
             <input type="submit" value="Acceder">
@@ -32,3 +31,4 @@ and open the template in the editor.
         ?>
     </body>
 </html>
+
